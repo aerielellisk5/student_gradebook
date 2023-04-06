3d16b411354e109fbbdb1854711a94606dbf5ee8
Aeriel <aerielellisk5@gmail.com>
2023-04-05 20:50:40 -0700

    added a few methods to find the average, highest and lowest scores

--- studentGrades.py
+++ studentGrades.py
@@ -7,7 +7,7 @@
 class Student:
     def __init__(self, name, grades):
         self.name = name
-        self.grades = []
+        self.grades = grades
 
             
     def add_grades(self, new_grade):
@@ -18,7 +18,41 @@ class Student:
             return formatted_grades
         else:
             return self.grades
+        
+    def average_grade(self, grades):
+        total = 0
+        for i in grades:
+            total = total + i
+        return total
+    
+    def highest_score(self, grades):
+        high_score = 0
+        for i in grades:
+            if i > high_score:
+                high_score = i
+            else:
+                continue
+        return high_score
+    
+    def lowest_score(self, grades):
+        lowest_score = 100 
+        for i in grades:
+            if i < lowest_score:
+                lowest_score = i
+            else:
+                continue
+        return lowest_score
+    
+    
+    
+            
+        
+# aeriel = Student("aeriel", [])
+# print("this is outside the method")
 
+# print(aeriel.average_grade([30, 45, 66, 200, 65]))
+# print(aeriel.highest_score([30, 45, 66, 22, 65]))
+# print(aeriel.lowest_score([30, 45, 66, 22, 65]))
 
 # class Classlist:
 #     def __init__(self, class_name, students):
@@ -46,7 +80,7 @@ while new_class == True:
         response_to_add_student = input("Would you like to add a new student to the gradebook?").lower()
         if response_to_add_student == "yes":
             name = input("What is the name of the student? ")
-            new_student = Student(name, grades= [])
+            new_student = Student(name, [])
             while add_student_grade == True:
                 new_grades = int(input("What is their grade on the exam? "))
                 new_student.add_grades(new_grades)
@@ -59,21 +93,21 @@ while new_class == True:
                     print(classroom)
                     print(str(new_student.grades))
                     break
+            print("does this ever get ran?")
         else:
             print("No worries, maybe another time!")
             if len(classroom) == 0:
                 print("This should exit the program")
-                new_class = False
-                break
+                # new_class = False
+                # add_student_status = False
             else:
                 for student, grade in classroom.items():
                     print(student + " is the student's name and their grades are: " + ','.join(str(x) for x in grade))                
-                    new_class = False
-                break
+            new_class = False
+            add_student_status = False
                 
             
     
-
+# teacher level --> class (math/english) --> students --> grades
+# writing to files to act like a database =) using json
             
-        
-    
